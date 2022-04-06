1a1d3ca513181b8e34ee3aaf7a8c716040ebc766
PetitYohan <yohan.petit@cpe.fr>
2022-04-06 12:03:02 +0200

    Atelier 2 : 2 et 3

--- src/view/GuiFactory.java
+++ src/view/GuiFactory.java
@@ -32,10 +32,10 @@ public class GuiFactory {
 		GUICoord gUICoord = new GUICoord(col, ligne);
 		
 		/* Version sans Decorator avec ou sans Observer  */
-		square = new SquareGui(gUICoord, squareColor);
+		//square = new SquareGui(gUICoord, squareColor);
 		
 		/* Version avec Decorator et Observer */
-//		square = new SquareGuiDecorator(new SquareGui(gUICoord, squareColor)); 
+		square = new SquareGuiDecorator(new SquareGui(gUICoord, squareColor));
 
 		return square;
 	}
---
+++ src/view/SquareGuiDecorator.java
@@ -0,0 +1,60 @@
+package src.view;
+
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
+import src.shared.GUICoord;
+
+public class SquareGuiDecorator extends BorderPane implements ChessSquareGui {
+
+    private SquareGui squareGui;
+
+    public SquareGuiDecorator(SquareGui squareGui) {
+        this.squareGui = squareGui;
+        changeListener();
+        paint();
+    }
+
+    private void changeListener() {
+        GuiConfig.paintStyle.addListener(new ChangeListener<PaintStyle>() {
+                 @Override
+                 public void changed(ObservableValue<? extends PaintStyle> observable, PaintStyle oldValue, PaintStyle newValue) {
+                     paint();
+                 }
+             }
+        );
+        GuiConfig.blackSquareColor.addListener(new ChangeListener<Color>() {
+            @Override
+            public void changed(ObservableValue<? extends Color> observable, Color oldValue, Color newValue) {
+                paint();
+            }
+        });
+        GuiConfig.whiteSquareColor.addListener(new ChangeListener<Color>() {
+            @Override
+            public void changed(ObservableValue<? extends Color> observable, Color oldValue, Color newValue) {
+                paint();
+            }
+        });
+    }
+
+    @Override
+    public GUICoord getCoord() {
+        return squareGui.getCoord();
+    }
+
+    @Override
+    public void resetColor(boolean isLight) {
+        squareGui.resetColor(isLight);
+        paint();
+
+    }
+
+    @Override
+    public void paint() {
+        squareGui.paint();
+        this.setBackground(squareGui.getBackground());
+        this.setBorder(squareGui.getBorder());
+    }
+}
